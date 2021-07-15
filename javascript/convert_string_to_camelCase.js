fb5dd31aedc82768843d429d31a6468a63f82b47
Daniel Ahn <iamdanahn@gmail.com>
2021-07-15 14:30:02 -0400

    Add test to toCamelCase problem

--- javascript/convert_string_to_camelCase.js
+++ javascript/convert_string_to_camelCase.js
@@ -26,3 +26,30 @@ function toCamelCase(str) {
 function capitalizeFirstLetter(word) {
 	return word[0].toUpperCase() + word.slice(1);
 }
+
+
+
+describe("Tests", () => {
+	it("test", () => {
+		Test.assertEquals(
+			toCamelCase(""),
+			"",
+			"An empty string was provided but not returned"
+		);
+		Test.assertEquals(
+			toCamelCase("the_stealth_warrior"),
+			"theStealthWarrior",
+			"toCamelCase('the_stealth_warrior') did not return correct value"
+		);
+		Test.assertEquals(
+			toCamelCase("The-Stealth-Warrior"),
+			"TheStealthWarrior",
+			"toCamelCase('The-Stealth-Warrior') did not return correct value"
+		);
+		Test.assertEquals(
+			toCamelCase("A-B-C"),
+			"ABC",
+			"toCamelCase('A-B-C') did not return correct value"
+		);
+	});
+});
